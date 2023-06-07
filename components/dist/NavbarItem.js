35675a646652e34a45fc7651fb7c8115fad67752
johncoh98 <John.coh98@gmail.com>
2023-06-07 20:29:58 +0300

    fbfdb

--- components/dist/NavbarItem.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-var NavbarItem = function (_a) {
-    var label = _a.label;
-    return (react_1["default"].createElement("div", { className: 'text-zinc-100 cursor-pointer hover:text-cyan-500 transition ' }, label));
-};
-exports["default"] = NavbarItem;
--- components/dist/PageLoader.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_loader_spinner_1 = require("react-loader-spinner");
-var PageLoader = function () {
-    return (React.createElement("div", { className: "flex items-center justify-center h-screen bg-black" },
-        React.createElement(react_loader_spinner_1.TailSpin, { height: 100, width: 100, color: "cyan" })));
-};
-exports["default"] = PageLoader;
