ebd0f6b85e02b3d0f06ca494dc5983656be19c5a
johncoh98 <John.coh98@gmail.com>
2023-06-07 20:30:59 +0300

    dvvdvd

--- pages/dist/scroll-triggered-text.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_intersection_observer_1 = require("react-intersection-observer");
-function ScrollTriggeredText() {
-    var _a = react_intersection_observer_1.useInView({
-        threshold: 0.5
-    }), ref = _a.ref, inView = _a.inView;
-}
